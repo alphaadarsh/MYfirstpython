230c2c01be33b64e0ebadaa083cf1bfed3ffbb11
alphaadarsh <alphaadarsh88@gmail.com>
2025-07-27 09:56:13 +0530

    my first python push

---
+++ Firstprogram.py
@@ -0,0 +1,18 @@
+# name_1 = "Adarsh"                         #   this is string string 
+# age = 17
+# price = 25.99  
+
+# print("My name is : " , name_1 )
+# print("my age is :", age)
+# print("my price : "  , 25.99  ) 
+
+# print (type(name_1))
+# print(type(age))
+# print(type(price)) 
+
+a = 27
+b = 54
+
+dif = a-b 
+print(dif)
+
---
+++ Operation.py
@@ -0,0 +1,35 @@
+#arithmetic operators
+a = 139
+b = 2
+
+print(a + b)
+print(a - b)
+print(a / b)
+print(a * b)
+print(a % b)
+print(a ** b)
+
+# comparison or relation 
+# i = 65 
+# j = 65 
+
+# print(i == j)
+# print(i != j)
+# print(a<= b)
+# print(a  >= b )
+# print(a > b)
+# print(a < b)
+
+# assignment operators 
+#num = 10 
+#num %= 10 
+
+#print("num : ", num  ) 
+
+#logical operators
+a  <= 15 
+b <= 30 
+
+print(a > 10 and b < 100) 
+print(a < 10 or b > 50)
+print(not(a < 10 or b > 50))
