51cabb0d54230c7c30b4fa63e512036802c9598f
alphaadarsh <alphaadarsh88@gmail.com>
2025-07-30 13:30:11 +0530

    third  commit

--- Operation.py
+++ Operation.py
@@ -1,27 +1,27 @@
 #arithmetic operators
-a = 139
+a = 10
 b = 2
 
 print(a + b)
 print(a - b)
 print(a / b)
 print(a * b)
-print(a % b)
+print(a % b)   #used for finding remainder 
 print(a ** b)
 
 # comparison or relation 
 # i = 65 
 # j = 65 
 
-# print(i == j)
-# print(i != j)
-# print(a<= b)
-# print(a  >= b )
+# print(i == j)  eqal to
+# print(i != j) not equal to 
+# print(a<= b)   b greater than or equal to a
+# print(a  >= b ) a greater than or equal to b
 # print(a > b)
 # print(a < b)
 
 # assignment operators 
-#num = 10 
+#num = 10  
 #num %= 10 
 
 #print("num : ", num  ) 
